760323eec1ab5cce8e61eb6e705942d45abedc82
asseko19 <asseko19@gmail.com>
2025-09-13 11:02:53 +0000

    Mise à jour de la configuration tel win

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ export default {
           'marquee': 'marquee 60s linear infinite',
           'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
           'fade-in': 'fadeIn 0.5s ease-in-out',
+          'slide-up': 'slideUp 1s ease-in-out',
       },
       keyframes: {
           marquee: {
@@ -31,6 +32,10 @@ export default {
           fadeIn: {
               '0%': { opacity: 0 },
               '100%': { opacity: 1 },
+          },
+          slideUp: {
+              '0%': { transform: 'translateY(20px)', opacity: 0 },
+              '100%': { transform: 'translateY(0)', opacity: 1 },
           },
       }
     }
