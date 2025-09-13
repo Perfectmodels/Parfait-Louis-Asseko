2d221879c6d94f7b0ec842b214714844ca68a515
asseko19 <asseko19@gmail.com>
2025-09-13 11:05:35 +0000

    Correction de la mise à jour des fichiers qui gère le design du site

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,9 @@ export default {
               '0%': { transform: 'translateY(20px)', opacity: 0 },
               '100%': { transform: 'translateY(0)', opacity: 1 },
           },
+      },
+      boxShadow: {
+        'gold': '0 0 20px 0px rgba(212, 175, 55, 0.5)',
       }
     }
   },
